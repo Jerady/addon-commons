5a0dc0df10e34c316eb32684e3df0c56486a19fd
Jens Deters <jens.deters@softblade.de>
2022-01-14 18:50:41 +0100

    release 16.0.0  (Java 16 ready) migration from junit 4 to junit 5

--- src/test/java/de/jensd/addon/AddonsRegistryTest.java
+++ src/test/java/de/jensd/addon/AddonsRegistryTest.java
@@ -1,64 +1,66 @@
-/**
- * Copyright (c) 2017 Jens Deters http://www.jensd.de
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- *
+/*
+  Copyright (c) 2017 Jens Deters http://www.jensd.de
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy of
+  the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  License for the specific language governing permissions and limitations under
+  the License.
  */
 package de.jensd.addon;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import de.jensd.addon.demo.converter.StringConverter;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 import java.net.URL;
 
 /**
  *
  * @author Jens Deters
  */
-public class AddonsRegistryTest {
+class AddonsRegistryTest {
 
     public AddonsRegistryTest() {
     }
 
     @Test
-    public void testLoadExtensions() {
+    void testLoadExtensions() {
         AddOnRegistryServiceLoader extensionRegistry = new AddOnRegistryServiceLoader();
         List<StringConverter> converter = extensionRegistry.getAddOns(StringConverter.class);
-        assertTrue(converter.size() == 3);
+        assertEquals(3, converter.size());
     }
 
     @Test
-    public void testConverter() {
+    void testConverter() {
         AddOnRegistryServiceLoader extensionRegistry = new AddOnRegistryServiceLoader();
         List<StringConverter> converter = extensionRegistry.getAddOns(StringConverter.class);
         Map<String, StringConverter> converters = converter.stream().collect(
-                Collectors.toMap(c -> c.name(), c -> c));
+                Collectors.toMap(StringConverter::name, c -> c));
         String lord = converters.get("DarkSideConverter").convert("Anakin Skywalker");
-        assertEquals(lord, "Darth Vader");
+        assertEquals("Darth Vader", lord);
         String jedi = converters.get("LightSideConverter").convert("Darth Vader");
-        assertEquals(jedi, "Anakin Skywalker");
+        assertEquals("Anakin Skywalker", jedi);
         String cast = converters.get("CastConverter").convert("Luke");
-        assertEquals(cast, "Mark Hamill");
+        assertEquals("Mark Hamill", cast);
     }
     
     @Test
-    public void testLoadExtensionsFromJar() {
+    void testLoadExtensionsFromJar() {
         String lookupPath = "build/resources/test";
         System.setProperty(AddOnRegistryServiceLoader.ADDON_LOOKUP_PATH_PROPERTY_NAME, lookupPath);
         AddOnRegistryServiceLoader extensionRegistry = new AddOnRegistryServiceLoader();
         List<URL> addOnUrls = extensionRegistry.lookupAddOnUrls();
+        assertEquals(1, addOnUrls.size());
     }
 }
